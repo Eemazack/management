a78af09ca5a390a1854e1cfddf1ca57d175a7779
eemazack <eemazack@gmail.com>
2022-07-17 23:54:23 +0800

    Add signup and login page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -13,14 +13,21 @@ import { SharedModule } from './shared/shared.module';
 import { FullComponent } from './layouts/full/full.component';
 import { AppHeaderComponent } from './layouts/full/header/header.component';
 import { AppSidebarComponent } from './layouts/full/sidebar/sidebar.component';
+import { HttpClientModule } from '@angular/common/http';
+import { SignupComponent } from './signup/signup.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { LoginComponent } from './login/login.component';
 @NgModule({
-  declarations: [	
+  declarations: [
     AppComponent,
     HomeComponent,
     BestSellerComponent,
     FullComponent,
     AppHeaderComponent,
-    AppSidebarComponent
+    AppSidebarComponent,
+    SignupComponent,
+    ForgotPasswordComponent,
+    LoginComponent
    ],
   imports: [
     BrowserModule,
@@ -30,7 +37,8 @@ import { AppSidebarComponent } from './layouts/full/sidebar/sidebar.component';
     ReactiveFormsModule,
     MaterialModule,
     FlexLayoutModule,
-    SharedModule
+    SharedModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { LoginComponent } from '../login/login.component';
+import { UserService } from '../services/user.service';
+import { SignupComponent } from '../signup/signup.component';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +12,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dialog: MatDialog,private router : Router,private userService:UserService) { }
 
   ngOnInit(): void {
+    if(localStorage.getItem('token') != null){
+      this.userService.checkToken().subscribe((res:any)=>{
+        this.router.navigate(['/cafe/dashboard']);
+      },(err)=>{
+        console.log(err);
+      })
+    }
+  }
+
+
+  signupAction(){
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.width = '550px';
+    this.dialog.open(SignupComponent,dialogConfig);
+  }
+
+  forgotPasswordAction(){
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.width = '550px';
+    this.dialog.open(SignupComponent,dialogConfig);
+  }
+
+
+  loginAction(){
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.width = '550px';
+    this.dialog.open(LoginComponent,dialogConfig);
   }
 
 }
+
