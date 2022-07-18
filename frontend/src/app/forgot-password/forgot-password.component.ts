62165bd92aa72a444dd5a8b3e8d7eac378243f03
eemazack <eemazack@gmail.com>
2022-07-18 11:03:01 +0800

    database update

--- frontend/src/app/forgot-password/forgot-password.component.ts
+++ frontend/src/app/forgot-password/forgot-password.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
+//import { NgxUiLoaderService } from 'ngx-ui-loader/lib/core/ngx-ui-loader.service';
+import { SnackbarService } from '../services/snackbar.service';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-forgot-password',
@@ -6,8 +12,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forgot-password.component.scss']
 })
 export class ForgotPasswordComponent implements OnInit {
+  forgotPasswordForm: any = FormGroup;
+  responseMessage : any;
 
-  constructor() { }
+  constructor(private formBuilder:FormBuilder, 
+    private userService: UserService,
+    private dialogRef: MatDialogRef<ForgotPasswordComponent>,
+    private ngxService: NgxUiLoaderService,
+    private snackbarService: SnackbarService
+    ) { }
 
   ngOnInit(): void {
   }
